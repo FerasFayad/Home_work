37c74e272adb1dbc74d8b984ecde0362f4328fc2
Feras <ferasfayad262@gmail.com>
2026-01-17 15:36:20 +0200

    q17 fixed

--- session1_Home_work/q17.dart
+++ session1_Home_work/q17.dart
@@ -2,3 +2,15 @@
 If you need a variable that can hold any type of value and may change during execution,
  which data type would you use? Write a code example to show this */
  
+/*Solution 
+You should use a dynamic variable,it allows you to change both its value and its data type during Run-Time.
+*/
+
+//Example
+void main(){
+dynamic generalVariable = "Feras Fayad"; //can be name as a String variable
+generalVariable = 27; //can be an age as Int variable
+generalVariable = 70.5; //can be an tall of human as a double variable
+generalVariable = true ; //can be a boolean variable as question
+//that happens because the dynamic variable typed at run-time
+}
