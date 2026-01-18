190513d14e5eb00fd1e5d22f084a75f32226d122
Feras <ferasfayad262@gmail.com>
2026-01-18 15:07:22 +0200

    question1 and question2 are done

---
+++ Seesion2_Home_Work/question1.dart
@@ -0,0 +1,13 @@
+/*Exercise 1:
+1. a) Create a Dart file with a main function.
+b) Inside main, print your name and your age on two separate lines.
+c) Print a single string that contains both name and age using string interpolation.
+ */
+void main(){
+  print('Feras\nFayad');
+  String firstName = 'Feras';
+  String lasName = 'Fayad';
+  int age = 27;
+  print('my name is a$firstName $lasName and my age is a $age');
+
+}
---
+++ Seesion2_Home_Work/question2.dart
@@ -0,0 +1,16 @@
+/*
+Exercise 2:
+2. a) Declare variables: String country, int year, double weight,
+ bool likesCoding. Assign values.
+  b) Print a sentence that includes all values using string interpolation.
+c) Change weight to a different value and print only the updated one. */
+void main (){
+  String counrty = 'Gaza City'; 
+  int year = 1998; 
+  double weight = 70;
+  bool likesCoding = true;
+  print("i'm from $counrty i was born in $year my weight is a $weight and yeb $likesCoding really engoy with coding");
+  weight = 75;
+  print('and now my weight is $weight');
+
+}
