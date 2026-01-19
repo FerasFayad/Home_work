3d0345ac0530adef88707abcd44330f849c4edbf
Feras <ferasfayad262@gmail.com>
2026-01-19 14:11:15 +0200

    question9 done

--- Seesion2_Home_Work/quiestion9.dart
+++ Seesion2_Home_Work/quiestion9.dart
@@ -1,4 +1,7 @@
 /*Exercise 9:
 9. a) Create List> students with two items, each having name and grade.
- b) Print the grade of the second student using index and key.
+b) Print the grade of the second student using index and key.
 c) Add both grades and print the average grade as double. */
+void main (){
+  List <String , double> students = {'Feras':9.5 , 'Khaled':7};
+}
