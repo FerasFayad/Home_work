7dfddb35a20823d63aa3eb8b409aafa5e04782e5
Feras <ferasfayad262@gmail.com>
2026-01-19 14:24:22 +0200

    last check is done

--- Seesion2_Home_Work/quiestion9.dart
+++ Seesion2_Home_Work/quiestion9.dart
@@ -3,11 +3,14 @@
 b) Print the grade of the second student using index and key.
 c) Add both grades and print the average grade as double. */
 void main (){
-  List <Map <String? , double?>> students = [{'Feras':9.5} ,{'Khled' : 8.0}];
+  List <Map <String , dynamic>> students = [
+    {'name' : 'Feras','grade' :9.5} ,
+    {'name':'Khled' ,'grade': 8.0}  ,
+    ];
   print(students[1]['Khled']);
 
-  //average = sum of values / numbers of values
-  double average = students[0]['Feras']! + students[1]['Khled']! / 2; 
+  //average = (sum of values ) / numbers of values
+  double average = (students[0]['grade']! + students[1]['grade']!) / students.length; 
   print(average);
 
  
