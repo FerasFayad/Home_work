200ba575ad65551770a01b3cb832d979f7163f2c
Feras <ferasfayad262@gmail.com>
2026-01-26 19:22:31 +0200

    question8 has fixed

---
+++ session4_Home_Work/question8.dart
@@ -0,0 +1,9 @@
+/*Q8
+Create a program with a setting called API_URL that is empty.
+ If it is empty, replace it with 'https://example.com'. 
+Print the new value in capital letters. */
+void main (){
+  String  API_URL = '';
+  API_URL.isEmpty ?print(API_URL='https://example.com'.toUpperCase()):null;
+
+}
