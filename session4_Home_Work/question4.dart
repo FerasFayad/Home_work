65bd3d8813e7a1d7c113e39d48e5f26e64d0b04d
Feras <ferasfayad262@gmail.com>
2026-01-26 18:03:31 +0200

    question4 fixed

---
+++ session4_Home_Work/question4.dart
@@ -0,0 +1,8 @@
+/*Q4
+Create a program with a list of names that is empty.
+ If the list has no items, print the message: No data available.
+ */
+void main(){
+  List names = [];
+names.isEmpty?print('No data available'): null;
+}
