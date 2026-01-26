221cb8a5c98ce16d5787c72ab5805d559b8d6709
Feras <ferasfayad262@gmail.com>
2026-01-26 19:57:22 +0200

    question 10 is done!!

---
+++ session4_Home_Work/question10.dart
@@ -0,0 +1,8 @@
+/*Q10
+Create a program with the numbers [3, 7, 2, 9, 12, 4, 6]. 
+Print only the numbers that are divisible by 3. */
+void main() {
+  List<int> numbers = [3, 7, 2, 9, 12, 4, 6];
+
+  numbers.where((number)=>number%3==0).forEach(print);
+}
