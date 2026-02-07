aab4a0bef55f87c19c6ddfc043dd9bccde720d6a
Feras <ferasfayad262@gmail.com>
2026-02-07 14:43:28 +0200

    question 5 last check

--- session6_Home_Work/question5.dart
+++ session6_Home_Work/question5.dart
@@ -7,8 +7,8 @@ import 'dart:io';
 void main(){
 List<int> numbers = getNumbers();
 print(numbers);
-int largestNumber = getLargestSecoundNumber(numbers);
-print(largestNumber);
+List<int> maxAndSceoundMaxNumbers = getLargestSecondNumber(numbers);
+maxAndSceoundMaxNumbers.forEach(print);
 }
 List<int> getNumbers(){
   return List.generate(6, (index){
@@ -17,8 +17,34 @@ List<int> getNumbers(){
   });
 }
 
-int getLargestSecoundNumber (List<int> numbers){
-  return  numbers.reduce((number1 ,number2){
-    return number1 >=number2 ? number1:number2;
-    });
+//[2,3,1,5,2,6]
+//prev = 2 3 3 5 5 6
+//curr = 3 1 5 2 6
+
+//prev = return Max number 3 3 5 5 6
+//curr = 1 secound Max number
+
+List<int> getLargestSecondNumber(List<int> numbers) {
+  if (numbers.length < 2) {
+    throw Exception('List must contain at least 2 numbers');
+  }
+
+  final result = numbers.skip(1).fold<List<int>>(
+    [numbers[0], numbers[0]], // [max, secondMax]
+    (acc, curr) {
+      int max = acc[0];
+      int secondMax = acc[1];
+
+      if (curr > max) {
+        secondMax = max;
+        max = curr;
+      } else if (curr > secondMax && curr != max) {
+        secondMax = curr;
+      }
+
+      return [max, secondMax];
+    },
+  );
+
+  return result;
 }
