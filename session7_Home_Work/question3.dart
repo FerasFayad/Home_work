112fe72f1bbe20da37da8420760049035e09109e
Feras <ferasfayad262@gmail.com>
2026-02-09 14:52:23 +0200

    question 3 is done

---
+++ session7_Home_Work/question3.dart
@@ -0,0 +1,28 @@
+/*Q3 Create a class Movie with attributes title and rating.
+ In main(), create a list of 4 movies. Print only the movies with a rating above 7.
+ */
+
+void main() {
+  Map<String, Movie> movies = {
+    'avatar': Movie('Avatar', 6.1),
+    'scream': Movie('Scream', 8.6),
+    'titanic': Movie('Titanic', 4.0),
+    'john_wick': Movie('John Wick', 9.5),
+  };
+
+  print(movies['john_wick']!.rating);
+
+  // طباعة الأفلام اللي تقييمها فوق 7
+  movies.forEach((key, movie) {
+    if (movie.rating > 7) {
+      print(movie.title);
+    }
+  });
+}
+
+class Movie {
+  String title;
+  double rating;
+
+  Movie(this.title, this.rating);
+}
