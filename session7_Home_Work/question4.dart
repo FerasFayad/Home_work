025c8aaa016b12f884168bd7f05f6e0d771c4b7c
Feras <ferasfayad262@gmail.com>
2026-02-09 15:03:39 +0200

    last check

---
+++ session7_Home_Work/question4.dart
@@ -0,0 +1,22 @@
+/*Q4
+  Create a class Employee with attributes name and salary.
+  Add a method giveRaise(int amount) that increases the salary.
+  In main(), create an employee, give them a raise, and print the new salary.
+ */
+
+void main() {
+  Employee employee = Employee('Ahmad', 90000);
+  employee.giveRaise(1000);
+  print(employee.salary);
+}
+
+class Employee {
+  String name;
+  int salary;
+
+  Employee(this.name, this.salary);
+
+  void giveRaise(int amount) {
+    salary += amount;
+  }
+}
