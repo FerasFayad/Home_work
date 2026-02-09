1a9124ce7ee2cc749ef241cb4e56bc4008149775
Feras <ferasfayad262@gmail.com>
2026-02-09 14:51:43 +0200

    question1 is done

--- session7_Home_Work/question1.dart
+++ session7_Home_Work/question1.dart
@@ -5,13 +5,17 @@
 void main() {
   City city1 = City();
   city1.name = 'Gaza';
-  city1.population = '';
+  city1.population = 2000000;
+
   City city2 = City();
-  city1.name = 'Cario';
-  city2.population = '';
+  city2.name = 'Cairo';
+  city2.population = 22000000;
+
+  print('City: ${city1.name}, Population: ${city1.population}');
+  print('City: ${city2.name}, Population: ${city2.population}');
 }
 
 class City {
   String? name;
-  String? population;
+  int? population;
 }
