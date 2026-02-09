a9d3bf940158e5b04de65126747e7a4fa32fcb05
Feras <ferasfayad262@gmail.com>
2026-02-09 15:21:41 +0200

    check and fixing

---
+++ session7_Home_Work/question7.dart
@@ -0,0 +1,6 @@
+/*
+Q7 
+Ask the user for a number (e.g., 9875).
+ Keep summing its digits until the result is a single digit.
+ Print the final single-digit result. (Example: 9+8+7+5 = 29 → 2+9 = 11 → 1+1 = 2)
+ */
---
+++ session7_Home_Work/question8.dart
@@ -0,0 +1,4 @@
+/*
+Q8 Ask the user to input a sentence.
+ Print all the words that appear only once in the sentence.
+ Also print the total count of unique words. */
