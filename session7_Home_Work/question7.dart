79641d27a2799d79195d1da80e9208895bd0a7c2
Feras <ferasfayad262@gmail.com>
2026-02-09 15:52:45 +0200

    checking

--- session7_Home_Work/question7.dart
+++ session7_Home_Work/question7.dart
@@ -4,3 +4,23 @@ Ask the user for a number (e.g., 9875).
  Keep summing its digits until the result is a single digit.
  Print the final single-digit result. (Example: 9+8+7+5 = 29 → 2+9 = 11 → 1+1 = 2)
  */
+import 'dart:io';
+
+void main() {}
+String inputNumber() {
+  String? input;
+  while (true) {
+    if (input == null) break;
+
+    try {
+      input = stdin.readLineSync()!;
+    } catch (e) {
+      print(e);
+    }
+  }
+  return input!;
+}
+
+List<String> splitNumber(String number) {
+  return number.trim().split(RegExp(r'/s+'));
+}
