a057e37c4b22f5fe9b5bfd2629c42e89d4e15198
Feras <ferasfayad262@gmail.com>
2026-02-10 14:37:53 +0200

    fixing some problemes and last check

--- session7_Home_Work/question8.dart
+++ session7_Home_Work/question8.dart
@@ -4,19 +4,39 @@ Q8 Ask the user to input a sentence.
  Also print the total count of unique words. */
 import 'dart:io';
 
+import 'dart:io';
+
 void main() {
+  String sentence = readSentence();
+  List<String> words = extractWords(sentence);
+  Map<String, int> wordCount = countWords(words);
+  int uniqueCount = printUniqueWords(wordCount);
+
+  print('Total unique words count: $uniqueCount');
+}
+
+String readSentence() {
   stdout.write('Enter a sentence: ');
-  String sentence = stdin.readLineSync()!;
+  return stdin.readLineSync()!;
+}
 
-  List<String> words = sentence.toLowerCase().trim().split(RegExp(r'\s+'));
+List<String> extractWords(String sentence) {
+  return sentence.toLowerCase().trim().split(RegExp(r'\s+'));
+}
 
+Map<String, int> countWords(List<String> words) {
   Map<String, int> wordCount = {};
 
   for (String word in words) {
     wordCount[word] = (wordCount[word] ?? 0) + 1;
   }
 
+  return wordCount;
+}
+
+int printUniqueWords(Map<String, int> wordCount) {
   print('Words that appear only once:');
+
   int uniqueCount = 0;
 
   wordCount.forEach((word, count) {
@@ -26,5 +46,5 @@ void main() {
     }
   });
 
-  print('Total unique words count: $uniqueCount');
+  return uniqueCount;
 }
